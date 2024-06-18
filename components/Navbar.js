dd9a75b6dddcb295fd2a460075fae98ab2b1d781
anisurarzu <anisur35-2163@diu.edu.bd>
2024-06-18 13:48:55 +0600

    first commit with project setup

---
+++ components/Navbar.js
@@ -0,0 +1,167 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { Dropdown, Input, Space } from "antd";
+import {
+  DownOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+  EnvironmentOutlined,
+  AppstoreOutlined,
+  FireOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const items = [
+    {
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com">
+          1st menu item
+        </a>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com">
+          2nd menu item
+        </a>
+      ),
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: "3rd menu item（disabled）",
+      key: "3",
+      disabled: true,
+    },
+  ];
+  return (
+    <nav className="bg-white  shadow-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div
+              className="flex-shrink-0 text-white p-2  rounded"
+              style={{ background: "#008BFF" }}>
+              <AppstoreOutlined
+                className="pr-2 "
+              
+              />
+              <Dropdown
+                menu={{
+                  items,
+                }}>
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    Browse All Categories
+                    <DownOutlined />
+                  </Space>
+                </a>
+              </Dropdown>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link
+                  href="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex">
+                  <FireOutlined className="pr-2"   style={{ color: "#008BFF"}} />
+                  <p>Deals</p>
+                </Link>
+                <Link
+                  href="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Home
+                </Link>
+                <Link
+                  href="/about"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  About
+                </Link>
+                <Link
+                  href="/services"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Services
+                </Link>
+                <Link
+                  href="/contact"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="bg-gray-900 text-gray-400 hover:text-white hover:bg-gray-800 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+              <svg
+                className="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24">
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <Link
+            href="/"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            About
+          </Link>
+          <Link
+            href="/services"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Services
+          </Link>
+          <Link
+            href="/contact"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ components/Topbar.js
@@ -0,0 +1,159 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import logo from "./images/logo.png";
+import Search from "antd/es/input/Search";
+import { Dropdown, Input, Space } from "antd";
+import {
+  DownOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+  EnvironmentOutlined,
+} from "@ant-design/icons";
+
+const Topbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onSearch = (value, _e, info) => console.log(info?.source, value);
+  const items = [
+    {
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com">
+          1st menu item
+        </a>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com">
+          2nd menu item
+        </a>
+      ),
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: "3rd menu item（disabled）",
+      key: "3",
+      disabled: true,
+    },
+  ];
+
+  return (
+    <nav style={{ background: "#008BFF" }} className=" p-4 h-[124px]">
+      <div className="container mx-auto flex justify-between items-center pt-[20px]">
+        <div className="text-white text-lg font-semibold text-center">
+          <Link href="/">
+            <img
+              className=""
+              src={
+                "https://s3-alpha-sig.figma.com/img/1ef4/f8af/f5b91a019f2b0dffec050fe6d76dcb9c?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=B3kH5pKExkP0N3voB1xWVL7xMuXy5hfHRS0hyzyAeWw2WMStTlciuyrr84FrHkuu~vtSoZ~gaZ7PZIfQl96PWUmx~N27AKO~U1c~4V3xep~OdVT3-ayYlrHfT3bdonK7YMw3U9hgXfaYTZUgarQ24aRgWJcxwRdwMxQ35GBef3J4AvrYgsPr9bPmA0BQNk6mFH4Ez4np8M8ikQnSpGSUBbyFh0uiEz5mmLVELyJZ3uRQGDI-nDhbY2VQHibFiqSPhzIxg4R6lYxWrF7xpVpbLfDb8YFAPzDqq906xhBWzTXdOa2GZfuYSIAYnsOqDpR~~ZhbB9-e5op5ixHBrjgL0A__"
+              }
+              alt=""
+            />
+          </Link>
+        </div>
+
+        <div className="border-0 rounded">
+          <div className="bg-white text-[12px] p-1 text-gray-500 w-[400px] h-[50px] mt-[0px] pt-2 border-0 rounded-sm">
+            <Dropdown
+              menu={{
+                items,
+              }}>
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  All Categories
+                  <DownOutlined />
+                </Space>
+              </a>
+            </Dropdown>
+
+            <Space direction="horizontal" className="">
+              <Input
+                placeholder="Search for items..."
+                variant="borderless"
+                size="large"
+                className="text-[12px]"
+              />
+              <SearchOutlined />
+            </Space>
+          </div>
+        </div>
+        <div className="bg-white text-[12px] text-gray-500 p-2  rounded ">
+          <EnvironmentOutlined className="pr-2" />
+          <Dropdown
+            menu={{
+              items,
+            }}>
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Your Location
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`md:flex items-center ${
+            isOpen ? "block" : "hidden"
+          } gap-4 text-white`}>
+          <div>Compare</div>
+          <div>Wishlist</div>
+          <div className="flex gap-1 ">
+            <div>
+              <ShoppingCartOutlined style={{ fontSize: "28px" }} />
+            </div>
+            <p className="text-[12px] pt-2">Cart</p>
+          </div>
+          <div className="flex gap-1">
+            <UserOutlined className="text-2xl" />
+
+            <p className="text-[12px] pt-2">Account</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Topbar;
